681b2a3d1fe4e6669f2b676fb2d474db2a85293f
Mark Abraham <mark.j.abraham@gmail.com>
2026-06-24 16:44:02 +0200

    Revert "Avoid structured bindings"
    This reverts commit 2791a8e652c8220dcbb8504a10ade76840ad656b.

--- src/gromacs/listed_forces/tests/bonded.cpp
+++ src/gromacs/listed_forces/tests/bonded.cpp
@@ -167,15 +167,9 @@ auto formatFepLambda = [](real lambda) { return formatString("Lambda%s", toStrin
  * it only controls what kinds of things are computed (forces only vs forces+virial+energy).
  * When the same quantity is computed by a different flavor, results should match.
  */
-const std::pair<NameOfTestFromTuple<ListedForcesTestHelper::DynamicParameters>,
-                RefDataFilenameMaker<ListedForcesTestHelper::DynamicParameters>>
-        sc_namers = ListedForcesTestHelper::makeNamers(
-                std::make_tuple(formatIListInput, formatCoordinates, formatPbcType, formatFepLambda),
-                std::make_tuple(formatFlavor));
-
-const NameOfTestFromTuple<ListedForcesTestHelper::DynamicParameters> sc_testNamer = sc_namers.first;
-const RefDataFilenameMaker<ListedForcesTestHelper::DynamicParameters> sc_refDataFilenameMaker =
-        sc_namers.second;
+const auto [sc_testNamer, sc_refDataFilenameMaker] = ListedForcesTestHelper::makeNamers(
+        std::make_tuple(formatIListInput, formatCoordinates, formatPbcType, formatFepLambda),
+        std::make_tuple(formatFlavor));
 
 /*! \brief Test fixture for listed forces
  *
